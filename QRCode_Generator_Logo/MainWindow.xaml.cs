e13c3ffaf653f2884769ac791c8924de4a5869d1
jerome jossent <jerome.jossent@veolia.com>
2024-06-14 11:28:11 +0200

    version .NET core en cours !

---
+++ QRCode_Generator_Logo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QRCode_Generator_Logo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            MessagingToolkit.QRCode.Codec.QRCodeEncoder encoder = new MessagingToolkit.QRCode.Codec.QRCodeEncoder();
+            MessagingToolkit.QRCode.Codec.QRCodeDecoder decoder = new MessagingToolkit.QRCode.Codec.QRCodeDecoder();
+            
+
+            string montexte = "Bonjour ceci est mon test :)çï";
+            System.Drawing.Bitmap im = encoder.Encode(montexte, Encoding.UTF8);
+
+            //MessagingToolkit.QRCode.Codec.Data.QRCodeImage ob = ;
+            //string lecture = decoder.Decode(ob); 
+        }
+    }
+}
